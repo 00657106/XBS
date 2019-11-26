a5758c0fd4830517e5e1b46ed259f034465dff6e
00657106 <55752394+00657106@users.noreply.github.com>
2019-11-26 15:22:19 +0800

    Create XBS_menuComboTable.php

---
+++ XBS_menuComboTable.php
@@ -0,0 +1,17 @@
+<?php
+    include_once 'menuDB.xbs.php';
+    $sql = "create table Combo(
+            ID          nvarchar(15)     not NULL,
+            name        nvarchar(20)     not NULL,
+            price       integer(4)      not NULL,
+            picture     varchar(200),
+            items       nvarchar(4000)  not NULL,
+            Primary key(ID)
+        );";
+    if (mysqli_query($conn, $sql)) {
+        echo "Table Combo created successfully";
+    } else {
+        echo "Creating Combo Error" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+?>
